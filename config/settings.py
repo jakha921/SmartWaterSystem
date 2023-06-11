26c4e73b8a6200258554ab8bea8ab86101e82633
jakha921 <jakha921@gmail.com>
2023-06-11 07:29:54 +0000

    add csrf access

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG == True:
+if DEBUG == False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
@@ -176,3 +176,7 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = "app.User"
 
 LOGIN_REDIRECT_URL = '/'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://smartsystem.uz',
+]
