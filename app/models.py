462ad8eb6e883173614bb9265f1964578b8f4e96
jakha921 <jakha921@gmail.com>
2024-02-26 21:47:12 +0500

    update to uzb lang

--- app/models.py
+++ app/models.py
@@ -61,6 +61,7 @@ class DeviceInfo(models.Model):
     device_number = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_drinkable = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.code} - {self.district}"
